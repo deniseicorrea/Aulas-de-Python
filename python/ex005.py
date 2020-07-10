cc14c405b5df5ed6db6226b2d886cea82f953939
Denise Correa <denisecorrea2803@yahoo.com.br>
2020-07-10 20:31:03 -0300

    novos exercicios

---
+++ python/ex005.py
@@ -0,0 +1,4 @@
+n = int(input('Digite um número:'))
+suc = n + 1
+ant = n - 1
+print(f'O sucessor de {n} é {suc} e seu antecessor é {ant}')
---
+++ python/ex006.py
@@ -0,0 +1,6 @@
+n = int(input('Digite um número: '))
+#dobro = n * 2, lembrando que eu poderia usar as váriaveis caso fosse necessário
+#triplo = n * 3
+#rq = n**(1/2)
+print(f'O dobro de {n} é {n*2}, o triplo {n*3} e a raiz quadrada {pow(n,(1/2)) :.2}')
+
