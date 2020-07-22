7af1c0929b5b2ea791346ce968582d44a8be8044
Denise Correa <denisecorrea2803@yahoo.com.br>
2020-07-22 16:26:21 -0300

    cores

---
+++ python/cores.py
@@ -0,0 +1,8 @@
+print('\033[1;31;43mOlá,mundo!\033[m')
+print('\033[4;30;45mOlá,mundo!\033[m')
+print('\033[7;30mOlá,mundo!\033[m')
+time = str('Dallas Cowboys')
+print(f'O melhor time de futebol americano é o \033[1;33m{time}\33[m')
+#cores = {'limpa': '\033[m' ,'amarelo':'\033[33m' ,'azul':'\033[34m'}#forma de lista
+elemento = str('amarelados')
+print(f'OS \033[34mgirassóis\033[m são de cor: \033[33m{elemento}')
