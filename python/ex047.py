11a32fe4ece87ea38592c7c9bf2a4345aa3b7686
Denise Correa <denisecorrea2803@yahoo.com.br>
2020-08-18 16:37:06 -0300

    novos exercicios

---
+++ python/ex047.py
@@ -0,0 +1,11 @@
+'''for c in range (1,51):
+    if c % 2 == 0:
+       print(c, end= ' ')
+print('Fim')'''  #Tem essa opçao e tem a logo abaixo que ocupa menas memoria#
+
+
+for c in range (2, 51, 2):
+    print('.', end= ' ')
+    print(c, end= ' ')
+print('Fim')
+
