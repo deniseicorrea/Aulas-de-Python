19b57fbdec95a5f105bf0c4daee4a8ec8dd78f62
Denise Correa <denisecorrea2803@yahoo.com.br>
2020-08-20 17:08:41 -0300

    novos exercicios

---
+++ python/ex052.py
@@ -0,0 +1,14 @@
+numero = int(input('Digite um número: '))
+tot = 0
+for c in range(1, numero + 1):
+    if numero % c == 0:
+        print('\033[33m', end= '')
+        tot += 1
+    else:
+        print('\033[31m', end= '')
+    print(f'{c}', end= ' ')
+print(f'\n\033[mO número {numero} foi divisível {tot} vezes')
+if tot == 2:
+    print('Número primo')
+else:
+    print('Não é número primo')
