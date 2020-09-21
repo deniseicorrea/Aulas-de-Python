64322ac519185350dade9f99d858f41a261b891b
Denise Correa <denisecorrea2803@yahoo.com.br>
2020-09-21 15:50:19 -0300

    novos exercicios

---
+++ python/ex069.py
@@ -0,0 +1,23 @@
+tot18 = toth = totm20 = 0
+while True:
+    idade = int(input('Idade: '))
+    sexo = ' '
+    while sexo not in 'MF':
+        sexo = str(input('Sexo: [M/F]')).strip().upper()[0]
+    if idade >= 18:
+        tot18 += 1
+    if sexo == 'M':
+        toth += 1
+    if sexo == 'F' and idade < 20:
+        totm20 += 1
+    resp = ' '
+    while resp not in 'SN':
+        resp = str(input('Deseja continuar? [S/N]')).strip().upper()[0]
+    if resp == 'N':
+       break
+print(f'O total de pessoas maior de 18 anos é {tot18}')
+print(f'Ao todo temos {toth} homens cadastrados')
+print(f'Temos {totm20} mulheres com menos de 20 anos')
+
+
+
